8a6782a8e1293b836a344a84f209e174e9ec18f9
Alejandro Cabeza <84131436+Alejocabeza@users.noreply.github.com>
2022-12-26 05:05:23 -0400

    Update mason-lsp.rc.lua

--- plugin/mason-lsp.rc.lua
+++ plugin/mason-lsp.rc.lua
@@ -1,15 +1,18 @@
-local status, mason_lsp = pcall(require, 'mason-lspconfig')
-if (not status) then return end
+local status, mason_lsp = pcall(require, "mason-lspconfig")
+if not status then
+	return
+end
 
 mason_lsp.setup({
-    ensure_installed = {
-        'bashls',
-        'diagnosticls',
-        'dockerls',
-        'intelephense',
-        'tsserver',
-        'vimls',
-        'yamlls',
-        'sumneko_lua'
-    }
+	ensure_installed = {
+		"bashls",
+		"diagnosticls",
+		"dockerls",
+		"intelephense",
+		"tsserver",
+		"vimls",
+		"yamlls",
+		"sumneko_lua",
+		"pyright",
+	},
 })
