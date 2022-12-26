21a3bc5878dc95d77a7b09fbcac799a0c23a948b
Alejandro Cabeza <84131436+Alejocabeza@users.noreply.github.com>
2022-12-26 05:04:54 -0400

    Update telescope.rc.lua

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -1,6 +1,8 @@
 local status, telescope = pcall(require, "telescope")
 
-if not status then return end
+if not status then
+	return
+end
 
 local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
@@ -8,6 +10,8 @@ local builtin = require("telescope.builtin")
 local function telescope_buffer_dir()
 	return vim.fn.expand("%:p:h")
 end
+
+local fb_actions = require("telescope").extensions.file_browser.actions
 
 telescope.setup({
 	defaults = {
@@ -29,17 +33,51 @@ telescope.setup({
 						vim.cmd("normal vbd")
 					end,
 				},
+				["n"] = {
+					-- your custom normal mode mappings
+					["N"] = fb_actions.create,
+					["h"] = fb_actions.goto_parent_dir,
+					["/"] = function()
+						vim.cmd("startinsert")
+					end,
+				},
 			},
 		},
 	},
 })
 
-vim.keymap.set("n", ";f", function() builtin.find_files({ no_ignore = false, hidden = true, }) end)
-vim.keymap.set("n", ";l", function() builtin.live_grep() end)
-vim.keymap.set("n", ";b", function() builtin.buffers() end)
-vim.keymap.set("n", ";t", function() builtin.help_tags() end)
-vim.keymap.set("n", ";r", function() builtin.resume() end)
-vim.keymap.set("n", ";e", function() builtin.diagnostics() end)
+telescope.load_extension("file_browser")
+
+vim.keymap.set("n", "sf", function()
+	telescope.extensions.file_browser.file_browser({
+		path = "%:p:h",
+		cwd = telescope_buffer_dir(),
+		respect_gitignore = false,
+		hidden = true,
+		grouped = true,
+		previewer = false,
+		initial_mode = "normal",
+		layout_config = { height = 40 },
+	})
+end)
+vim.keymap.set("n", ";f", function()
+	builtin.find_files({ no_ignore = false, hidden = true })
+end)
+vim.keymap.set("n", ";l", function()
+	builtin.live_grep()
+end)
+vim.keymap.set("n", ";b", function()
+	builtin.buffers()
+end)
+vim.keymap.set("n", ";t", function()
+	builtin.help_tags()
+end)
+vim.keymap.set("n", ";r", function()
+	builtin.resume()
+end)
+vim.keymap.set("n", ";e", function()
+	builtin.diagnostics()
+end)
 -- Command for git
 vim.keymap.set("n", ";gc", "<cmd>Telescope git_commits<cr>")
 vim.keymap.set("n", ";gfc", "<cmd>Telescope git_bcommits<cr>")
