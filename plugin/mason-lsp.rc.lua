8aa3f3bf9a77571050db98ce02557b0948a43923
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2023-03-11 14:57:34 -0500

    changes in mason-lsp plugin

--- plugin/mason-lsp.rc.lua
+++ plugin/mason-lsp.rc.lua
@@ -1,26 +1,26 @@
 local status, mason_lsp = pcall(require, "mason-lspconfig")
 if not status then
-	return
+    return
 end
 
 mason_lsp.setup({
-	ensure_installed = {
-		"bashls",
-		"diagnosticls",
-		"dockerls",
-		"intelephense",
-		"tsserver",
-		"vimls",
-		"yamlls",
-		"lua_ls",
-		"pyright",
-		"tailwindcss",
-		"docker_compose_language_service",
-		"clangd",
-		"cssls",
-		"html",
-		"eslint",
-		"taplo",
-	},
-	automatic_installation = false,
+    ensure_installed = {
+        "bashls",
+        "diagnosticls",
+        "dockerls",
+        "intelephense",
+        "tsserver",
+        "vimls",
+        "yamlls",
+        "lua_ls",
+        "pyright",
+        "tailwindcss",
+        "docker_compose_language_service",
+        "clangd",
+        "cssls",
+        "html",
+        "eslint",
+        "taplo",
+    },
+    automatic_installation = true,
 })
